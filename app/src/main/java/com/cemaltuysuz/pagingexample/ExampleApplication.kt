b8a062cdc0ec2290b68b18e0c3952c69695baa42
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-30 12:50:39 +0300

    created paths for folders | Application class for Hilt

---
+++ app/src/main/java/com/cemaltuysuz/pagingexample/ExampleApplication.kt
@@ -0,0 +1,8 @@
+package com.cemaltuysuz.pagingexample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExampleApplication() : Application() {
+}
